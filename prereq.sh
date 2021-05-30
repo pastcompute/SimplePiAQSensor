e86480038f14c38e0e3f41b05d311f227aefccfc
Andrew McDonnell <bugs@andrewmcdonnell.net>
2021-05-30 11:02:17 +0930

    Fixup prereq script

--- prereq.sh
+++ prereq.sh
@@ -1,9 +1,13 @@
-sudo apt install mosquitto_clients python3-pip gcc cmake i2c-tools
+#!/bin/bash
+set -e
+
+# Note, mosquitto is optional, it runs a local MQTT service which is connected to by default
+sudo apt-get install -y mosquitto-clients python3-pip gcc cmake i2c-tools python3-pip python3-pil mosquitto
 pip3 install adafruit_circuitpython_sgp30
 pip3 install adafruit_circuitpython_ssd1306
-git clone https://github.com/LuckyResistor/ReadSGP30
+test -e ReadSGP30/.git || git clone https://github.com/LuckyResistor/ReadSGP30
 cd ReadSGP30
-mkdir -p buid
+mkdir -p build
 cd build
 cmake ..
 make
